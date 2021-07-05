7ee3d55849596109fcb15cabe79452a0fb07b81a
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-05 17:06:19 +0500

    setup: Update the setup.py file

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+setup(
+    author='Bekhruz Tuychiev',
+    description='A package to convert Jupyter Notebooks to Streamlit web apps.',
+    name='streamlitbook',
+    version='0.1.0',
+    packages=find_packages(include=['streamlitbook', 'streamlitbook.*']),
+    install_requires=['streamlit>=0.80.0', 'numpy', 'pandas'],
+    python_requires='>=3.7'
+)
