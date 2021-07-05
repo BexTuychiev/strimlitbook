c5b98ac0a2279332357027897da3219cd9e9007a
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-05 16:00:40 +0500

    setup: Initial setup of files and dirs

--- streamlitbook/__init__.py
+++ streamlitbook/__init__.py
@@ -5,3 +5,5 @@ Convert Jupyter Notebooks to Streamlit Web Apps
 A complete package to parse and display Jupyter Notebook
 cells in Streamlit web app scripts. Identical conversion.
 """
+
+from streamlitbook import parse
---
+++ streamlitbook/parse/__init__.py
@@ -0,0 +1,7 @@
+"""
+A subpackage to read and parse Jupyter Notebooks.
+"""
+
+
+def dummy_function():
+    print("Hello")
