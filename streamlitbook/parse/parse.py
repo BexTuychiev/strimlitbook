edcc993d7ed137d8e445dce2d72e871317b3c399
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-05 18:38:35 +0500

    feature: Build the initial constructor for the Notebook class

--- streamlitbook/parse/parse.py
+++ streamlitbook/parse/parse.py
@@ -1,10 +1,17 @@
 """
 A module that contains classes to deal with Jupyter Notebooks
 """
+import json
 
 
 class Notebook:
-    pass
+
+    def __init__(self, path):
+        with open(path, 'rb') as file:
+            data_dict = json.load(file)
+        self.cells = data_dict['cells']
+        self.n_cells = len(data_dict['cells'])
+        self.metadata = data_dict['metadata']
 
 
 class Cell:
