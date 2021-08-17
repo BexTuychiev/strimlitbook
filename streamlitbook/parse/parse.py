d62747dbd8d62208404581228c4e74443dd669b8
Bex Tuychiev <bex@ibexprogramming.com>
2021-08-17 14:49:56 +0500

    enhance: Make an empty list the default tag attribute for cells with no tags

--- streamlitbook/parse/parse.py
+++ streamlitbook/parse/parse.py
@@ -59,7 +59,7 @@ class Cell:
         self._type = cell_dict['cell_type']
         self._metadata = cell_dict['metadata']
         self._source = "".join(cell_dict['source'])
-        self._tags = self._metadata.get("tags", None)
+        self._tags = self._metadata.get("tags", [])
 
     @property
     def type(self):
