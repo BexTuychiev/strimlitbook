5ccbccf36a50e9d72247f4ea6a6df28ec19250fe
BexTuychiev <bex@ibexprogramming.com>
2022-01-26 21:08:54 +0500

    refactor: Add a new file for utility functions

---
+++ streamlitbook/utils.py
@@ -0,0 +1,15 @@
+"""
+A module that contains utility functions.
+"""
+
+from . import StreamlitBook
+
+import base64
+import re
+from PIL import Image
+import io
+
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.graph_objects as go
