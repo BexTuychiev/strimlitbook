a7befdf1b70c7d9372643e7999cbadcf84d4b886
BexTuychiev <bex@ibexprogramming.com>
2022-01-29 08:36:03 +0500

    refactor: Enhance parse_plotly_output function with a new variable for the plotly key

--- streamlitbook/parse/outputs.py
+++ streamlitbook/parse/outputs.py
@@ -15,14 +15,15 @@ def parse_plotly_output(output):
     # If output type is either display_data or execute_result, the output is media type
     if output['output_type'] in ("display_data", "execute_result"):
         # If the below key exists, the output is Plotly figure
-        if "application/vnd.plotly.v1+json" in output['data'].keys():
-            plotly_data = output['data']['application/vnd.plotly.v1+json']['data']
-            plotly_layout = output['data']['application/vnd.plotly.v1+json']['layout']
+        plotly_key = "application/vnd.plotly.v1+json"
+        if plotly_key in output['data'].keys():
+            plotly_data = output['data'][plotly_key]['data']
+            plotly_layout = output['data'][plotly_key]['layout']
 
             # If config key exists in Plotly output dict,
             # user passed custom config to the chart
-            if "config" in output['data']['application/vnd.plotly.v1+json'].keys():
-                plotly_config = output['data']['application/vnd.plotly.v1+json']['config']
+            if "config" in output['data'][plotly_key].keys():
+                plotly_config = output['data'][plotly_key]['config']
             else:
                 plotly_config = None
 
