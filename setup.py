cb9da530512e567c7e8a2f05f2b056b0c282da70
BexTuychiev <bex@ibexprogramming.com>
2022-07-07 16:30:30 +0500

    docs: Update setup.py and requirements.txt files

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
-    author='Bekhruz Tuychiev',
+    author='Bekhruz (Bex) Tuychiev',
+    email='bex@ibexprogramming.com',
+    license="MIT",
     description='A package to convert Jupyter Notebooks to Streamlit web apps.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/BexTuychiev/streamlitbook',
     name='streamlitbook',
     version='0.1.0',
     packages=find_packages(include=['streamlitbook', 'streamlitbook.*']),
-    install_requires=['streamlit>=0.80.0', 'numpy', 'pandas'],
+    install_requires=['streamlit', 'numpy', 'pandas', 'plotly', 'Pillow'],
     python_requires='>=3.7'
 )
