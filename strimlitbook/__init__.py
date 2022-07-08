e037f6ee381adbbed424f561962a06e075ca250a
BexTuychiev <bex@ibexprogramming.com>
2022-07-08 15:17:09 +0500

    docs: Add a pyproject.toml file

--- strimlitbook/__init__.py
+++ strimlitbook/__init__.py
@@ -5,6 +5,7 @@ Convert Jupyter Notebooks to Streamlit Web Apps
 A complete package to parse and display Jupyter Notebook
 cells in Streamlit web app scripts. Identical conversion.
 """
+__version__ = "0.1.0"
 
 from .parse import StreamlitBook
 from .reader import read_ipynb
