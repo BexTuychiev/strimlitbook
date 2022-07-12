05f1a612fd8ad8e67ac12ce82988ec3eb19a025a
BexTuychiev <bex@ibexprogramming.com>
2022-07-12 22:25:38 +0500

    docs: Fill out the docstrings of functions inside outputs.py

--- strimlitbook/parse/outputs.py
+++ strimlitbook/parse/outputs.py
@@ -1,4 +1,25 @@
 def _parse_stream_output(output):
+    """
+    Parse a text output of a code cell.
+    The output_type in the notebook JSON is given as "stream".
+
+    Parameters
+    ----------
+    output : dict
+        A single code cell output as a dictionary.
+
+    Returns
+    -------
+        parsed_output : dict
+            A dictionary containing the parsed output with "stdout" key.
+
+    Notes
+    -----
+        The difference between this function and _parse_plain_text_output is that this
+        function only parses outputs of the Python print function.
+        While _parse_plain_text_output parses all text outputs from executing the code
+        cell by running Shift + Enter or Ctrl + Enter commands.
+    """
     parsed_output = dict()
 
     if output['output_type'] == "stream":
@@ -10,6 +31,23 @@ def _parse_stream_output(output):
 
 
 def _parse_plotly_output(output):
+    """
+    Parse a Plotly HTML code of a code cell.
+    The output_type in the notebook JSON is given as "application/vnd.plotly.v1+json"
+    under the "data" key.
+
+    Parameters
+    ----------
+    output : dict
+        A single code cell output as a dictionary.
+        
+
+    Returns
+    -------
+        parsed_output : dict
+            A dictionary containing the parsed output with a dictionary of data,
+            layout, and config items under the "plotly_fig" key.
+    """
     parsed_output = dict()
 
     # If output type is either display_data or execute_result, the output is media type
@@ -38,6 +76,20 @@ def _parse_plotly_output(output):
 
 
 def _parse_html_output(output):
+    """
+    Parse HTML of a code cell. The output_type in the notebook JSON is given as
+    "text/html" under the "data" key.
+
+    Parameters
+    ----------
+    output : dict
+        A single code cell output as a dictionary.
+
+    Returns
+    -------
+        parsed_output : dict
+            A dictionary containing the parsed output with "text/html" key.
+    """
     parsed_output = dict()
     plotly_key = "application/vnd.plotly.v1+json"
 
@@ -54,6 +106,20 @@ def _parse_html_output(output):
 
 
 def _parse_image_output(output):
+    """
+    Parse binary image data of a code cell. The output_type in the notebook JSON is given
+    as "image/png" under the "data" key.
+
+    Parameters
+    ----------
+    output : dict
+        A single code cell output as a dictionary.
+
+    Returns
+    -------
+        parsed_output : dict
+            A dictionary containing the parsed output with "image/png" key.
+    """
     parsed_output = dict()
     plotly_key = "application/vnd.plotly.v1+json"
 
@@ -70,6 +136,28 @@ def _parse_image_output(output):
 
 
 def _parse_plain_text_output(output):
+    """
+    Parse a text output of a code cell after it was run with Shift + Enter or Ctrl + Enter
+    commands. See notes on the difference between _parse_stream_output and this function.
+    The output_type in the notebook JSON is given as "text/plain".
+
+    Parameters
+    ----------
+    output : dict
+        A single code cell output as a dictionary.
+
+    Returns
+    -------
+        parsed_output : dict
+            A dictionary containing the parsed output with "text/plain" key.
+
+    Notes
+    -----
+        The difference between this function and _parse_stream_output is that
+        _parse_stream_output function only parses outputs of the Python print function.
+        While this function parses all text outputs from executing the code
+        cell by running Shift + Enter or Ctrl + Enter commands.
+    """
     parsed_output = dict()
 
     if output['output_type'] in ("display_data", "execute_result"):
@@ -85,6 +173,20 @@ def _parse_plain_text_output(output):
 
 
 def _parse_error_output(output):
+    """
+    Parse an error output of a code cell. The output_type in the notebook JSON is given as
+    "error" and the error name is under "ename".
+
+    Parameters
+    ----------
+    output : dict
+        A single code cell output as a dictionary.
+
+    Returns
+    -------
+        parsed_output : dict
+            A dictionary containing the parsed output with "error" key.
+    """
     parsed_output = dict()
 
     if output['output_type'] == "error":
